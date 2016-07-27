e346420d9cbf2562b98e830848bd703d5c7f6597
muraliselvaraj <s.murali@live.in>
2016-07-27 16:33:08 +0530

    packages added and user login and logout apis created

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,5 +7,44 @@
 
 module.exports = {
 	
+	signup: function(req, res){
+		if(!req.body || !req.body.name || !req.body.email || !req.body.password){
+			return res.negotiate({status: 400, message: 'required fields are missing'});
+        } else {
+        	User.signup(req.body, function(err, user){
+				if(err){
+					return res.negotiate(err);
+				} else {
+					return res.json(user);
+				}
+			});
+        }
+	},
+
+	login: function(req, res){
+		if(!req.body || !req.body.email || !req.body.password){
+			return res.negotiate({status: 400, message: 'required fields are missing'});
+        } else {
+	    	User.login(req.body, function(err, user){
+	    		if(err){
+	    			return res.negotiate(err);
+	    		} else {
+	    			return res.json(user);
+	    		}
+	    	});
+	    }
+	},
+
+	logout: function(req, res){
+		var id = req.user.id;
+		User.edit(id, {loggedIn: 0}, function(err, response){
+			if(err){
+				return res.negotiate(err);
+			} else {
+				return res.json({message: 'User logged out successfully'});
+			}
+		});
+	}
+
 };
 
--- api/models/User.js
+++ api/models/User.js
@@ -4,11 +4,90 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 module.exports = {
 
-  attributes: {
+	attributes: {
+		name: {
+			type: 'string',
+			max: 16
+	    },
+	    email: {
+	    	type: 'email',
+	    	unique: true,
+	    	required: true
+	    },
+	    password: {
+			type: 'string'
+	    },
+	    loggedIn: {
+			type: 'boolean',
+			defaultsTo: 0,
+			required: true
+	    }
+	},
 
-  }
+	beforeCreate: function(values, next){
+		if(values.password){
+			var salt = bcrypt.genSaltSync(10);
+			bcrypt.hash(values.password, salt, function (err, hash) {
+				if(err){
+					return next(err);
+				} else {
+					values.password = hash;
+					next();
+				}
+			});
+		} else {
+			next();
+		}
+	},
+
+	signup: function(opts, cb){
+		User.create(opts, function(err, createdUser){
+			if(err){
+				return cb(err);
+			} else {
+				return cb(null, {message: 'user created successfully'});
+			}
+		});
+	},
+
+	login: function(opts, cb){
+		User.findOne({email: opts.email}).exec(function(err, user){
+			if(err){
+				return cb(err);
+			} else if(!user){
+				return cb({status: 400, message: 'User not found'});
+			} else {
+				var match = bcrypt.compareSync(opts.password, user.password);
+				if(match){
+					User.edit(user.id, {loggedIn: 1}, function(err, updatedUser){
+						if(err){
+							return cb(err);
+						} else {
+							var token = jwt.sign(user, 'secret', {expiresIn: 60*24*30});
+							updatedUser.token = token;
+							return cb(null, updatedUser);
+						}
+					});
+				} else {
+					return cb({status: 400, message: 'Email or password is incorrect'});
+				}
+			}
+		});
+	},
+
+	edit: function(id, body, cb){
+		User.update({id: id}, body, function(err, updatedUser){
+			if(err){
+				return cb(err);
+			} else {
+				return cb(null, updatedUser[0]);
+			}
+		});
+	}
+
 };
 
