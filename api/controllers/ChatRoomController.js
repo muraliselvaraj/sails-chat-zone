516d12a0c5d93b9a12d6ac2883b88335125b93f5
muraliselvaraj <s.murali@live.in>
2016-07-27 15:33:55 +0530

    api files created

---
+++ api/controllers/ChatRoomController.js
@@ -0,0 +1,11 @@
+/**
+ * ChatRoomController
+ *
+ * @description :: Server-side logic for managing Chatrooms
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
